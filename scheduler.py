858108a3220bb5b2240be2c9de01fcafd13ff5e3
Bhuvibagga <bhuvibagga1221@gmail.com>
2026-03-27 15:03:00 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,15 +1,38 @@
 import threading
 
-workers=[("localhost",5001)]
-current_worker=0
+# -------------------------------
+# WORKER LIST (specialized)
+# -------------------------------
+pdf_workers = [("localhost", 5001)]   # PDF → TXT
+txt_workers = [("localhost", 5002)]   # TXT → PDF
+
+pdf_index = 0
+txt_index = 0
+
 lock = threading.Lock()
 
-def get_next_worker():
-    global current_worker
+
+# -------------------------------
+# SCHEDULER FUNCTION
+# -------------------------------
+def get_next_worker(filename, target_format):
+    global pdf_index, txt_index
+
     with lock:
-        worker=workers[current_worker]
-        current_worker=(current_worker+1)%len(workers)
-        return worker
+        # PDF → TXT
+        if filename.endswith(".pdf") and target_format == "txt":
+            worker = pdf_workers[pdf_index]
+            pdf_index = (pdf_index + 1) % len(pdf_workers)
+            return worker
 
+        # TXT → PDF
+        elif filename.endswith(".txt") and target_format == "pdf":
+            worker = txt_workers[txt_index]
+            txt_index = (txt_index + 1) % len(txt_workers)
+            return worker
 
+        else:
+            raise ValueError("Unsupported conversion type")
+
+    
     
